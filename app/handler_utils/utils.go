b4238cb8e64a60285b6c41c5d2df4df7a8eae925
zix-apple <ZixMai@users.noreply.github.com>
2025-08-06 15:44:18 +0300

    feat: add markdown v2 support

---
+++ app/handler_utils/utils.go
@@ -0,0 +1,89 @@
+package handler_utils
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+type Placeholder struct {
+	ID    string
+	Value string
+}
+
+func escapeMarkdownV2Base(text string) string {
+	special := "_*[]()~`>#+-=|{}.!"
+	var builder strings.Builder
+
+	for _, r := range text {
+		if r >= 1 && r <= 126 {
+			if strings.ContainsRune(`\`+special, r) {
+				builder.WriteRune('\\')
+			}
+		}
+		builder.WriteRune(r)
+	}
+	return builder.String()
+}
+
+func escapeInlineCode(code string) string {
+	code = strings.ReplaceAll(code, `\`, `\\`)
+	code = strings.ReplaceAll(code, "`", "\\`")
+	return "`" + code + "`"
+}
+
+func escapePreBlock(code string) string {
+	code = strings.ReplaceAll(code, `\`, `\\`)
+	code = strings.ReplaceAll(code, "`", "\\`")
+	return "```\n" + code + "\n```"
+}
+
+func escapeURL(url string) string {
+	url = strings.ReplaceAll(url, `\`, `\\`)
+	url = strings.ReplaceAll(url, ")", "\\)")
+	return url
+}
+
+func EscapeMarkdownV2Smart(input string) string {
+	var placeholders []Placeholder
+	placeholderCount := 0
+
+	makePlaceholder := func(replacement string) string {
+		id := fmt.Sprintf("\u0001%d\u0001", placeholderCount)
+		placeholders = append(placeholders, Placeholder{id, replacement})
+		placeholderCount++
+		return id
+	}
+
+	rePre := regexp.MustCompile("(?s)```(.*?)```")
+	input = rePre.ReplaceAllStringFunc(input, func(match string) string {
+		content := rePre.FindStringSubmatch(match)[1]
+		return makePlaceholder(escapePreBlock(content))
+	})
+
+	reInlineCode := regexp.MustCompile("(?s)`([^`\n]+?)`")
+	input = reInlineCode.ReplaceAllStringFunc(input, func(match string) string {
+		content := reInlineCode.FindStringSubmatch(match)[1]
+		return makePlaceholder(escapeInlineCode(content))
+	})
+
+	reLinks := regexp.MustCompile(`\[(.+?)\]\((.+?)\)`)
+	input = reLinks.ReplaceAllStringFunc(input, func(match string) string {
+		matches := reLinks.FindStringSubmatch(match)
+		escapedText := escapeMarkdownV2Base(matches[1])
+		escapedURL := escapeURL(matches[2])
+		return makePlaceholder("[" + escapedText + "](" + escapedURL + ")")
+	})
+
+	input = escapeMarkdownV2Base(input)
+	for _, ph := range placeholders {
+		input = strings.ReplaceAll(input, ph.ID, ph.Value)
+	}
+
+	return input
+}
+
+func MentionUser(name string, userID int64) string {
+	escapedName := escapeMarkdownV2Base(name)
+	return fmt.Sprintf("[%s](tg://user?id=%d)", escapedName, userID)
+}
--- app/handlers/start_command.go
+++ app/handlers/start_command.go
@@ -1,16 +1,17 @@
 package handlers
 
 import (
-	"fmt"
 	"github.com/mymmrac/telego"
 	th "github.com/mymmrac/telego/telegohandler"
 	tu "github.com/mymmrac/telego/telegoutil"
+	hndUtils "github.com/unspokenteam/golang-tg-dbot/app/handler_utils"
 	"middlewares"
 )
 
 func Start(ctx *th.Context, upd telego.Update) {
-	middlewares.MessageQueue <- tu.Message(
-		tu.ID(upd.Message.Chat.ID),
-		fmt.Sprintf("Hello %s!", upd.Message.From.FirstName),
-	)
+	middlewares.MessageQueue <- &telego.SendMessageParams{
+		ChatID:    tu.ID(upd.Message.Chat.ID),
+		Text:      hndUtils.EscapeMarkdownV2Smart("") + hndUtils.MentionUser(upd.Message.From.Username, upd.Message.From.ID),
+		ParseMode: telego.ModeMarkdownV2,
+	}
 }
