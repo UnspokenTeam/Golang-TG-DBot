bf9b5a51882dbc6ef65594edb8e1778b6d378890
zix-apple <ZixMai@users.noreply.github.com>
2026-02-09 02:27:13 +0300

    fix: typo

--- internal/bot/handlers/help_command.go
+++ internal/bot/handlers/help_command.go
@@ -2,8 +2,6 @@ package handlers
 
 import (
 	"context"
-	"fmt"
-	"log/slog"
 
 	"github.com/mymmrac/telego"
 	"github.com/unspokenteam/golang-tg-dbot/internal/bot/roles"
@@ -15,7 +13,6 @@ func Help(ctx context.Context, upd telego.Update, services *service_wrapper.Serv
 	text := services.ConfigCache.GetString("help_command_text")
 
 	if user, findErr := services.PostgresClient.Queries.GetUserByTgId(ctx, upd.Message.From.ID); findErr != nil {
-		slog.ErrorContext(ctx, fmt.Sprintf("user find error: %v", findErr))
 		return
 	} else {
 		if roles.Role(user.UserRole) == roles.OWNER {
@@ -25,7 +22,7 @@ func Help(ctx context.Context, upd telego.Update, services *service_wrapper.Serv
   /admin_stats - Приватная статистика за последние сутки. Команда также доступна админам.
   /promote - Повысить пользователя до администратора __(применяется в ответ на сообщение)__
   /demote - Понизить администратора до пользователя __(применяется в ответ на сообщение)__
-  /analyze - Получить ссылку на логи по конкретному пользователю и чаты в Uptrace. Команда также доступна админам. __(применяется в ответ на сообщение)__
+  /analyze - Получить ссылку на логи по конкретному пользователю и чату в Uptrace. Команда также доступна админам. __(применяется в ответ на сообщение)__
   /echo %s - Проверить как бот видит шаблон сообщения
   /send %s - Сделать глобальную рассылку по шаблону`
 		}
