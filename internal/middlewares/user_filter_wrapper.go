58d98bf00e85a2c6c700d5a339d4794a94adf989
zix-apple <ZixMai@users.noreply.github.com>
2026-02-08 22:15:16 +0300

    feat: mute counter

--- internal/middlewares/user_filter_wrapper.go
+++ internal/middlewares/user_filter_wrapper.go
@@ -11,7 +11,10 @@ import (
 	"github.com/unspokenteam/golang-tg-dbot/internal/bot/channels"
 	"github.com/unspokenteam/golang-tg-dbot/internal/bot/service_wrapper"
 	"github.com/unspokenteam/golang-tg-dbot/internal/configs"
+	"github.com/unspokenteam/golang-tg-dbot/internal/logger"
 	"github.com/unspokenteam/golang-tg-dbot/pkg/utils"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
 
 	"github.com/mymmrac/telego"
 	th "github.com/mymmrac/telego/telegohandler"
@@ -26,8 +29,9 @@ const (
 )
 
 var (
-	services *service_wrapper.Services
-	cfg      configs.RateLimiterConfig
+	services    *service_wrapper.Services
+	muteCounter metric.Int64Counter
+	cfg         configs.RateLimiterConfig
 )
 
 func makeKey(chatID, userID int64) string {
@@ -68,6 +72,13 @@ func isUserDeniedByRateLimit(ctx *th.Context, message *telego.Message) bool {
 		case LOUD:
 			tryIncrUserState(ctx.Context(), redisId, userState, time.Duration(cfg.SpamCooldown)*time.Second, message)
 			slog.DebugContext(ctx, fmt.Sprintf("Muting user %d...", message.From.ID), "payload", message)
+
+			go muteCounter.Add(ctx, 1, metric.WithAttributes(
+				attribute.Int64("user_id", message.From.ID),
+				attribute.Int64("chat_id", message.Chat.ID),
+				attribute.String("msg", fmt.Sprintf("%+v", message)),
+			))
+
 			go utils.TryMuteSpammer(ctx, message, cfg.SpamCooldown, services.TgApiRateLimiter)
 		default:
 		}
@@ -78,6 +89,17 @@ func isUserDeniedByRateLimit(ctx *th.Context, message *telego.Message) bool {
 func UserFilterWrapper(wrapper *service_wrapper.Services) func(*th.Context, telego.Update) error {
 	services = wrapper
 	cfg = configs.LoadConfig(services.AppViper, configs.RateLimiterConfig{})
+
+	var err error
+	muteCounter, err = services.Meter.Int64Counter(
+		"bot.mute.total",
+		metric.WithDescription("Total number of mutes in rate limiter"),
+		metric.WithUnit("1"),
+	)
+	if err != nil {
+		logger.Fatal(fmt.Sprintf("create metric err: %s", err))
+		return nil
+	}
 
 	return func(ctx *th.Context, upd telego.Update) error {
 		msg := upd.Message
